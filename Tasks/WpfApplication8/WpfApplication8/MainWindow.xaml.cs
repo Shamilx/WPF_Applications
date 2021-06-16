2f039d124444ad883a8e37a3152cb621c6328390
Shamil <77068688+Shamilx@users.noreply.github.com>
2021-06-16 18:47:11 +0400

    Add files via upload

---
+++ Tasks/WpfApplication8/WpfApplication8/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication8
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+
+            // CreateButtons();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button b = sender as Button;
+            this.Background = b.Foreground;
+        }
+
+        //public void CreateButtons()
+        //{
+        //    for (int i = 0; i < 10; i++)
+        //    {
+        //        Button bt = new Button();
+        //        SolidColorBrush brush = new SolidColorBrush();
+
+        //        Random rnd = new Random();
+
+        //        bt.Width = 100;
+        //        bt.Height = 25;
+
+        //        byte a = (byte)rnd.Next(256);
+        //        byte b = (byte)rnd.Next(256);
+        //        byte c = (byte)rnd.Next(256);
+        //        byte d = (byte)rnd.Next(256);
+
+        //        Color randomColor = Color.FromArgb(a, b, 25, d);
+
+        //        bt.Content = "Button";
+        //        brush.Color = randomColor;
+
+        //        bt.Foreground = brush;
+
+        //        StackPanel1.Children.Add(bt);
+        //    }
+        //}
+    }
+}
